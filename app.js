5d08d2403dc09fddea18f6d80581835e79c87ca2
Samandar <samandarmamurov0409@gmail.com>
2026-04-16 14:21:10 +0500

    js masalalar add

---
+++ app.js
@@ -0,0 +1,12 @@
+// 1- masala                 sonning juft yoki toq son ekanligini aniqlovchi dastur yozing
+let son = prompt("sonni kiriting");
+son = Number(son);
+
+if(son % 2 ===0) {
+    console.log("juft son");
+} else{
+    console.log("toq son");
+}
+
+// ==================================================================================================================
+
